398bd55ee7a75701c4b69eb58073d617659f901f
Erick Almeida | Barros Ealmeida Advogados <ealmeida@barros.adv.br>
2025-03-19 13:42:53 -0300

    feat: Checar e Notificar o termino do Loop

--- robo.py
+++ robo.py
@@ -2,8 +2,8 @@ import pyautogui as pag
 import time
 import os
 import shutil
-import pyperclip
 import pygetwindow as gw
+from win10toast import ToastNotifier
 
 #coordenadas dos elementos da tela
 LUPA = (1415, 180) #Apenas na página de inclusão de anexos
@@ -18,38 +18,42 @@ PRIMEIRO_ARQUIVO = (431, 263)
 ANEXOS_AREA =(1198, 599)
 
 #abre o diretorio pai ✅
-diretorio_pai = 'A:/TI/Processamento Cadastro DRC/Anexos parte 2 - 22.01'
+diretorio_pai = 'C:/Users/ealmeida/Desktop/CADASTRO 19.03.2025/ANEXOS'
 
 diretorio_feito = os.path.join(diretorio_pai, 'Anexos feitos')
 itens = os.listdir(diretorio_pai)
 diretorios_filhos = [item for item in itens if os.path.isdir(os.path.join(diretorio_pai, item))]
 
-
 # Cria o diretório "Anexos feitos" se não existir
 os.makedirs(diretorio_feito, exist_ok=True)
+
+mensagem = ToastNotifier()
 
 def ponteiro_de_anexos():
     for i in range(len(diretorios_filhos)):
         try:
             diretorio = diretorios_filhos[i] #escolhe um diretorio
             global nome_gcpj
-            nome_gcpj = diretorio #nome da janela do explorer
+            nome_gcpj = diretorio #nome do diretorio
 
-            time.sleep(.5)
+            #checa se é o diretório de um GCPJ
+            if nome_gcpj.isnumeric():
+                pass
+            else:
+                mensagem.show_toast("Anexos Concluídos",f"Diretorio {nome_gcpj} não é um GCPJ válido", duration=2)
+                print(f"Diretorio {nome_gcpj} não é um GCPJ válido")
+                break
             
-            # (AQUI) função para rodar o robo
-            rodar_robo()
+            time.sleep(.5)
+            rodar_robo() #função para navegar no Octopus
 
             # Move o diretório processado para o diretório "Anexos feitos" 
             shutil.move(os.path.join(diretorio_pai, diretorio), diretorio_feito)
-            print(f"Anexos do {diretorio} movidos para {diretorio_feito}")
+            print(f"\n{diretorio} concluído e movido para {diretorio_feito}\n")
             
         except Exception as e:
+            mensagem.show_toast("ERROR",f"Erro ao processar o diretório {diretorio}", duration=2)
             print(f"Erro ao processar o diretório {diretorio}: {e}")
-            print("Deseja continuar com o próximo diretório? (s/n)")
-            if input().lower() != 's':
-                print("Processamento interrompido pelo usuário.")
-                break
             print("Continuando com o próximo diretório...")
             time.sleep(2)
             continue
@@ -57,7 +61,6 @@ def ponteiro_de_anexos():
 #pesquisa o GCPJ e clica no link do processo
 def pesquisar_gcpj(nome_gcpj):
     time.sleep(1)
-    # pag.hotkey("alt", "tab")
     time.sleep(1)
     pag.click(LUPA, duration=.5)
     time.sleep(1)
@@ -69,8 +72,6 @@ def pesquisar_gcpj(nome_gcpj):
     pag.press("enter")
     time.sleep(7)
     pag.click(LINK_GCPJ, duration=.5)
-    # scroll_down()
-    # anexar()
 
 #procura o botão pra seguir para a página de anexos
 def scroll_down():
@@ -88,7 +89,7 @@ def anexar():
     pag.click(ANEXOS_BUTTON, duration=.5)
     time.sleep(1)
     pag.click(CATEGORIAS_ANEXOS, duration=.5)
-    time.sleep(1)
+    time.sleep(3)
     pag.press("down", presses=11)
     pag.press("enter")
     time.sleep(1)
@@ -108,7 +109,7 @@ def arrastar(diretorio_pai,nome_gcpj):
     pag.moveTo(ANEXOS_AREA, duration=.5)
     pag.hotkey("alt", "tab")
     pag.mouseUp()
-    time.sleep(3)
+    time.sleep(5)
     pag.click(LUPA, duration=.5)
 
 #fechar janela de anexos ✅
@@ -116,10 +117,11 @@ def fechar_janela(nome_gcpj):
     janelas = gw.getWindowsWithTitle(nome_gcpj) 
     if janelas:
         janelas[0].close()
-        print(f"Janela {janelas[0]} fechada com sucesso.")
     else:
-        print("janela não encontrada")
+        mensagem.show_toast("ERROR",f"janela '{nome_gcpj}' não encontrada", duration=2)
+        print("Erro: janela não encontrada")
 
+#Navegar no Octopus
 def rodar_robo():
     pesquisar_gcpj(nome_gcpj)
     scroll_down()
@@ -127,6 +129,6 @@ def rodar_robo():
     arrastar(diretorio_pai,nome_gcpj)
     fechar_janela(nome_gcpj)
 
-#play
+#Run
 pag.hotkey("alt", "tab")
 ponteiro_de_anexos()
